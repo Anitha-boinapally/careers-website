cb6e63f5ea88530a9041d1dac6d9f3f303770a27
Anitha-boinapally <133632356+Anitha-boinapally@users.noreply.github.com>
2024-02-07 08:26:06 +0000

    added html home page with dynamic

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
 print("Hello")
-from flask import Flask
+from flask import Flask,render_template,jsonify
 app=Flask(__name__)
+JOBS=[
+{
+  'id':1,
+  'Role':'Data Analyst',
+  'location':'Hyd,India'
+},
+{
+  'id':2,
+  'Role':'Data Analyst',
+  'location':'Hyd,India'
+},
+{
+  'id':3,
+  'Role':'Data Analyst',
+  'location':'Hyd,India'
+}
+]
 @app.route("/")
 def hello_world():
-   return "Hello Anusha!"
+   return render_template('home.html',jobs=JOBS)
+@app.route("/jobs")
+def list_of_jobs():
+  return jsonify(JOBS)
 print(__name__)
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
